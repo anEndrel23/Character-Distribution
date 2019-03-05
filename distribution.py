2891460338ca941daaaa62e8008ae2dfcdd88214
anEndrel23 Hhs <44811950+anEndrel23@users.noreply.github.com>
2019-03-05 14:16:46 -0500

    Updated from Brython Server: 3/5/2019 2:16:45 PM

--- distribution.py
+++ distribution.py
@@ -38,6 +38,7 @@ Notice about this example:
 """
 import string
 stg = str(input("Please enter a string of text (the bigger the better): "))
+print('The distribution of characters in ''"' + stg + '" is:')
 stg = stg.lower()
 
 letters = list(string.ascii_lowercase)
